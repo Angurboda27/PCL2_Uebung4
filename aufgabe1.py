88b63215f8d8af7757cc56951addfbdd29daa378
Nora Lötscher <Nora@Noras-MacBook-Air.local>
2017-04-25 22:55:04 +0200

    uploaded final scripts and pdf file

--- aufgabe1.py
+++ aufgabe1.py
@@ -1,24 +1,100 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# PCL II, Übung 4, FS17
-# Aufgabe 1
-# Autor: Nora Lötscher & Alex Flückiger
-# Matrikel-Nr.: 09-110-552 & 12-452-223
+# PCL II, exercise 04, spring 2017
+# Task 1
+# Authors: Nora Lötscher & Alex Flückiger
+# Student-ID.: 09-110-552 & 12-452-223
 
 """
-Script extracts the 20 most frequent sentences from a corpus into an output file.
+This module provides functions to extract the 20 most common sentences
+from multiple XML-files. To achieve this in an efficient way, XML-files
+are parsed iteravely as well as generators and hashes are used.
 """
+
+import glob
+from lxml import etree as ET
+
 
 def getfreqwords(indir, outfile):
-	# indir is an xml file
-	# outfile is a .txt file
+    """
+    This functions coordinates the extractions of sentences from multiple
+    XML-files. Each unique sentence gets hashed and will be counted
+    over the entire corpus. The 20 most frequent sentences will be written
+    into a textfile.
 
-# iterate through xml file and build lemmatised sentences out of word lemmas
-# find 20 most frequent sentences that are AT LEAST 6 TOKENS LONG (write into output file)
+    Args:
+        indir (str): directory that contains XML-files
+        outfile (str): textfile in which most common sentences will be written
+
+    Return:
+        None
+    """
+    count_sents = dict()
+    # Asterisk is used as wildcard.
+    file_pattern = indir + 'SAC-Jahrbuch_*_mul.xml'
+    files = glob.glob(file_pattern)
+    for file in files:
+        for sent_lemm in extract_sents(file):
+            unique_id = hash(sent_lemm)
+            if unique_id in count_sents:
+                counter = count_sents[unique_id][1]
+                count_sents[unique_id] = (sent_lemm, counter + 1)
+            else:
+                count_sents[unique_id] = (sent_lemm, 1)
+
+    # write 20 most common sentences into textfile
+    with open(outfile, 'w', encoding='utf-8') as out_f:
+        for key in sorted(count_sents, key=lambda x: count_sents[x][1],
+                          reverse=True)[:20]:
+            out_f.write(count_sents[key][0] + '\n')
+            print(count_sents[key])
+
+
+def extract_sents(file):
+    """
+    This function iterates over all sentences of an XML-file and
+    yields each sentence consisting of lemmatized forms.
+    However, only sentences with a minimu of 6 tokens are returned.
+
+    Args:
+        file (str): name of XML-file
+
+    Yield:
+        sent_processed (str): extracted sentence
+    """
+
+    for _, sent in ET.iterparse(file, tag='s'):
+        current_sent = []
+        for word in sent.iterfind('.//w'):
+            try:
+                lemma = word.attrib['lemma']
+                current_sent.append(lemma)
+            except KeyError:
+                # Continue if lemma does not exist for a specific word
+                # In our SAC corpus there are several Swiss german words
+                # without lemmas
+                pass
+
+        if len(current_sent) >= 6:
+            yield ' '.join(current_sent)
+
+        # Some cleaning up to prevent memory issues
+        sent.clear()
+        # Delete parent node of sentences if there is more than one sentence
+        # Does not have any significant impact in this example
+        while sent.getprevious() is not None:
+            del sent.getparent()[0]
+
 
 def main():
-    getfreqwords()
+    """
+    Call functions to test this module.
+    """
+
+    indir = 'Text+Berg_Release_152_v01/Corpus_XML/SAC/'
+    outfile = 'frequent_lemmatized_sentences.txt'
+    getfreqwords(indir, outfile)
 
 
 if __name__ == '__main__':
--- aufgabe2.py
+++ aufgabe2.py
@@ -1,41 +1,111 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# PCL II, Übung 4, FS17
-# Aufgabe 2
-# Autor: Nora Lötscher & Alex Flückiger
-# Matrikel-Nr.: 09-110-552 & 12-452-223
+# PCL II, exercise 04, spring 2017
+# Task 2
+# Authors: Nora Lötscher & Alex Flückiger
+# Student-ID.: 09-110-552 & 12-452-223
 
 """
-Script extracts random elements into output file and writes rest into another file.
+This module provides functions to download a Wikipedia dump and sample k titles
+out of all article with the Reservoir Sampling algorithm.
 """
 
+import urllib.request
+import lxml.etree as ET
+import bz2
 import random
+import sys
+
+
+def progress_reporting(count, blocksize, totalsize):
+    """
+    This function reports the progress of downloaded blocks in console
+
+    Args:
+        count (int)
+        blocksize (int)
+        totalsize (int)
+    """
+
+    percent = int(count * blocksize / totalsize * 100)
+    sys.stdout.write(f'\rDowloaded {percent:0d}% of requested file.')
+    sys.stdout.flush()
+
+
+def corpus_download(src_url, trg_xml):
+    """
+    This function downloads a source file from the internet and saves it locally.
+
+    Args:
+        src_url (str): URL of remote location
+        trg_xml (str): local destination to save file
+    """
+
+    with open(trg_xml, 'w', encoding='utf-8') as f_xml:
+        urllib.request.urlretrieve(
+            src_url, filename=trg_xml, reporthook=progress_reporting)
+        print(f'Corpus was downloaded successfully\n {trg_xml}')
+
 
 def gettitles(infile, testfile, trainfile, k):
-	# infile is corpus
-	# testfile is output file with k randomly selected titles
-	# trainfile is output file with rest of titles
-	
-	"""
-	Reservoir Sampling:
-	
-	reservoir = []
-	for t, item in enumerate(iterable):
-		if t < k:
-			reservoir.append(item)
-		else:
-			m = random.randint(0,t)
-			if m < k:
-				reservoir[m] = item
-	return reservoir
-	"""
+    """
+    This functions iterates over Wikipedia-Dump and saves k random items
+    into a testfile and all other elements into a trainfile.
 
-# iterate through xml file and build lemmatised sentences out of word lemmas
-# find 20 most frequent sentences that are AT LEAST 6 TOKENS LONG (write into output file)
+    As sampling method the algorithm R is used (reservoir sampling).
+
+    Args:
+        infile (str or filehandler): file from which will be sampled
+        testfile (str): file in which k random elements will be written
+        trainfile (str): file in which all other elements will be written
+        k (int): number of sample
+    """
+
+    # reservoir shows currently sampled items
+    reservoir = []
+    # t counts seen elements regardless whether they are sampled or not
+    t = 0
+    with open(testfile, 'w', encoding='utf-8') as f_test, \
+            open(trainfile, 'w', encoding='utf-8') as f_train:
+        elements = ET.iterparse(infile)
+        for _, element in elements:
+            if element.tag == '{http://www.mediawiki.org/xml/export-0.10/}title':
+                if t < k:
+                    reservoir.append(element.text)
+                else:
+                    m = random.randint(0, t)
+                    if m < k:
+                        f_train.write(reservoir[m] + '\n')
+                        reservoir[m] = element.text
+                    else:
+                        f_train.write(element.text + '\n')
+                t += 1
+
+            # Some cleaning up to prevent memory issues
+            element.clear()
+            while element.getprevious() is not None:
+                del element.getparent()[0]
+
+        # write sampled items into file
+        for article in reservoir:
+            f_test.write(article + '\n')
+
 
 def main():
-    gettitles()
+    """
+    Call functions to test script.
+    """
+
+    src_url = 'https://dumps.wikimedia.org/dewiki/latest/dewiki-latest-pages-articles.xml.bz2'
+    trg_xml = 'dewiki-latest-pages-articles.xml.bz2'
+    testfile = 'wikipedia_articles_testfile.txt'
+    trainfile = 'wikipedia_articles_trainfile.txt'
+
+    # corpus_download(src_url, trg_xml)
+
+    with bz2.open(trg_xml, mode='r') as f_corpus:
+        gettitles(f_corpus, testfile, trainfile, 26)
 
 
 if __name__ == '__main__':
